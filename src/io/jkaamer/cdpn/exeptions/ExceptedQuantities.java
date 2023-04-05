4cfe106e59a9b38faada5b7f1cf9f1e5f835a4ac
MAHDI HOSEIN ZADE <mahdihoseinzade.jk@gmail.com>
2023-04-05 14:33:35 +0430

    Create ExceptedQuantities.java

---
+++ src/io/jkaamer/cdpn/exeptions/ExceptedQuantities.java
@@ -0,0 +1,8 @@
+package io.jkaamer.cdpn.exeptions;
+
+public class ExceptedQuantities extends Exception {
+    // TODO Unfinished work must be finished: 1
+    public ExceptedQuantities(String message) {
+        super(message);
+    }
+}
