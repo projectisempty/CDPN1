6e6b24ba61e0272d1f09974665f2e540fabb0f94
MAHDI HOSEIN ZADE <mahdihoseinzade.jk@gmail.com>
2023-05-02 13:32:44 +0430

    Delete ExceptedQuantities.java

--- src/io/jkaamer/cdpn/exeptions/ExceptedQuantities.java
+++
@@ -1,8 +0,0 @@
-package io.jkaamer.cdpn.exeptions;
-
-public class ExceptedQuantities extends Exception {
-    // TODO Unfinished work must be finished: 1
-    public ExceptedQuantities(String message) {
-        super(message);
-    }
-}
